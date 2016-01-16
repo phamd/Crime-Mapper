95ef6ba1f8a016a4b466e74bbf2ac6088e4ea43f
s7aesis <ryancd774@gmail.com>
2016-01-16 17:17:58 -0500

    Now able to query for crime information

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,24 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    request = require('request'),
+    cheerio = require('cheerio'),
+    secrets = require('../../secrets.json');
 
 module.exports = router;
 
 router.get('/map', function(req, res){
    console.log('Map page'); 
+   
+   var url = 'http://api.spotcrime.com/crimes.json?lat=43.657799&lon=-79.71153&radius=0.2&callback=?&key='+secrets.spotcrime_api_key;
+   
+   request(url, function(err, result){
+      if(!err) {
+          res.end(result.body.substring(2, result.body.length-3));
+      } 
+      else {
+        res.end({"server error": "data not retrieved"});
+      }
+   });
+   
 });
+
