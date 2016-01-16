b9e90c6a40f126635b1be0f5be94aeea69a817a6
s7aesis <ryancd774@gmail.com>
2016-01-16 17:46:58 -0500

    Refactoring

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,5 @@
 var express = require('express'),
     router = express.Router(),
-    request = require('request'),
-    cheerio = require('cheerio'),
     secrets = require('../../secrets.json'),
     crime_data = require('../crime_data');
 
@@ -10,17 +8,8 @@ module.exports = router;
 router.get('/map', function(req, res){
    console.log('Map page'); 
    
-   var url = 'http://api.spotcrime.com/crimes.json?lat=43.657799&lon=-79.71153&radius=0.2&callback=?&key='+secrets.spotcrime_api_key;
-   
-   request(url, function(err, result){
-      if(!err) {
-          crime_data.getCrimes([43.657799, -79.71153], function(data){
-              res.end(data);
-          });
-      } 
-      else {
-        res.end({"server error": "data not retrieved"});
-      }
+   crime_data.getCrimes([43.657799, -79.71153], function(data){
+     res.end(data);
    });
 });
 
